d589a0536f049d236c96a9731592b5b287483dfe
Vamshi Bachaneboina <vamshi.bachaneboina@192.168.1.5>
2023-04-23 04:32:28 -0700

    serverless express-lambda setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gym-mates
+
 Gym Mates to help gym buddies to track their progress through analytics in whatsapp
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +22,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(express.json()); // Add this line to parse JSON request bodies
+
+// Your existing Express.js app code...
+
+// Add the /subscriptions endpoint
+app.post('/register-subscriptions', (req, res) => {
+  // Perform subscription registration logic
+  // Replace this with your actual implementation
+  const responseMessage = `Subscription registered for ${req.body.email}`;
+
+  res.json({ message: responseMessage });
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
